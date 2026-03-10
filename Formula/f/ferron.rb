68f08b68fe5ce5c4855544bcddc8564eefc85872
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-10 08:09:56 +0000

    ferron 2.6.0

--- Formula/f/ferron.rb
+++ Formula/f/ferron.rb
@@ -1,8 +1,8 @@
 class Ferron < Formula
   desc "Fast, memory-safe web server written in Rust"
   homepage "https://www.ferronweb.org/"
-  url "https://github.com/ferronweb/ferron/archive/refs/tags/2.5.5.tar.gz"
-  sha256 "e4bfd513d87ce32043c9f28cfb74b58c8b610f610bba79c59a0ff57370e0578c"
+  url "https://github.com/ferronweb/ferron/archive/refs/tags/2.6.0.tar.gz"
+  sha256 "50e874e4cefa8ac3601b171ffd694c13bef87306ef341b00973de180a3b1355b"
   license "MIT"
   head "https://github.com/ferronweb/ferron.git", branch: "develop-2.x"
 
